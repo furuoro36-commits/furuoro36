f54736e65b0e063503f69e48e504279ec8dd3df5
furuoro36-commits <furuoro36@gmail.com>
2025-12-18 17:41:44 +0900

    Update MBTeaI_js.js

--- MBTeaI_js.js
+++ MBTeaI_js.js
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const btn_q2 =document.querySelector('#btn_q2');
 
     let a=0
-    let b=0
     let c=0
+    let mondaisuu = 9
     //問題文格納配列
     const mondaibun = [
         '都会と田舎なら都会の方が好き',
@@ -23,6 +23,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
         '問題文４',
         '問題文５',
         '問題文６',
+        '問題文７',
+        '問題文８',
+        '問題文９',
         '診断は終了です！'
     ];
 
@@ -36,27 +39,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
         a+=1
         document.querySelector('#question').textContent = mondaibun[c];
         c+=1
-        if (c == 7){
+        if (c == mondaisuu + 1){
         document.querySelector("#btn_fi").disabled = false;
         document.getElementById('btn_fi').style.display = 'inline'
         document.querySelector("#btn_q1").disabled = true;
         document.querySelector("#btn_q2").disabled = true;
-        document.getElementById('#btn_q1').style.display = 'none'
-        document.getElementById('#btn_q2').style.display = 'none'
+        let k = a.toString();
+        sessionStorage.setItem('shindankekka',k );
     }
     }
     //✖ボタンコード
     const tyousa_batu = () => {
-        b+=1
         document.querySelector('#question').textContent = mondaibun[c];
         c+=1
-        if (c == 7){
+        if (c == mondaisuu + 1){
         document.querySelector("#btn_fi").disabled = false;
-        document.getElementById('btn_fi').style.display = 'inline'
+        document.getElementById('btn_fi').style.display = 'inline';
         document.querySelector("#btn_q1").disabled = true;
         document.querySelector("#btn_q2").disabled = true;
-        document.getElementById('#btn_q1').style.display = 'none'
-        document.getElementById('#btn_q2').style.display = 'none'
+        let k = a.toString();
+        sessionStorage.setItem('shindankekka',k );
     }
     }
 
@@ -75,27 +77,39 @@ document.addEventListener('DOMContentLoaded', (event) => {
 document.addEventListener('DOMContentLoaded', (event) => {
     const btn_fini =document.querySelector('#btn_fini');
     //診断結果の格納配列
-    const MBTeaI =[
-        '１種類目',
-        '２種類目',
-        '３種類目',
-        '４種類目',
-        '５種類目',
-        '６種類目',
-        '１種類目',
-        '２種類目',
-        '３種類目',
-        '４種類目',
-        '５種類目',
-        '６種類目',
-
+    const MBTeaI = [
+        '〇を０回押した',
+        '〇を１回押した',
+        '〇を２回押した',
+        '〇を３回押した',
+        '〇を４回押した',
+        '〇を５回押した',
+        '〇を６回押した',
+        '〇を７回押した',
+        '〇を８回押した',
+        '〇を９回押した',
     ]
     //お茶屋さんの格納配列
-    //診断結果を表示するコード
+    const otyayasan_gazou = [
+        'IMAGE/IMAGE_3730.jpg',
+        'IMAGE/club_logo.png',
+        'IMAGE/1755773016761.jpg',
+        'IMAGE/logo.png',
+        'IMAGE/スクリーンショット 2025-11-25 214845.png',
+        'IMAGE/スクリーンショット 2025-11-25 214845.png',
+        'IMAGE/1755773016761.jpg',
+    ]
+    //診断結果を表示するコード 
+    let kekka = Number(sessionStorage.getItem('shindankekka'));
+    document.querySelector('#kekka_hyouzi').textContent = MBTeaI[kekka];
+    //const kekka_Image = document.getElementById('#otyaya_gazou');
+    //kekka_Image.src = otyayasan_gazou[kekka];
     //ホーム画面に戻るコード
     const go_home = () => {
         window.location.href = "index.html";;
+        kekka = 0
+        k = 0
+        sessionStorage.removeItem('shindankekka');
     }
     btn_fini.addEventListener('click',go_home);
 });
-//ホーム画面のコード
